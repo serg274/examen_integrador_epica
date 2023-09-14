23a6143558ef6ef0e8141b43e9c52e7b94ca8367
sergio <66255714+serg274@users.noreply.github.com>
2023-09-14 19:57:38 +0200

    se agrega .env

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 const express = require("express");
-const cors = require('cors');
-const morgan = require('morgan');
-
+const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config();
 
 const app = express();
-const port = 5001;
+const port = process.env;
 
 //middleware
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
-
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
 
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
 app.post("/user", function (req, res) {
-
-  const {name, lasname , id} = req.body
-  res.send("Hello sergio");
+  //recibir datos por body
+  const { name, lastname, id } = req.body;
+  res.send({
+    name,
+    lastname,
+    id,
+  });
 });
 
 app.listen(port, () => console.log(`servidor en http://localhost:${port}`));
